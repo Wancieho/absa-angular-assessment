db49c270d80a4750afd328cfdb79e96055593028
Wancieho <wancieho@gmail.com>
2020-07-06 21:48:17 +0200

    remove module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-// import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-// import { NgxBootstrapIconsModule, allIcons } from 'ngx-bootstrap-icons';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,8 +10,6 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    // NgbModule,
-    // NgxBootstrapIconsModule.pick(allIcons),
     AppRoutingModule,
   ],
   bootstrap: [
